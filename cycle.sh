76317230a7b4e4284ab99337cdbcfc74b1ca05d2
Horea Christian <chr@chymera.eu>
2022-12-14 04:30:34 -0500

    Only using jpg images

--- cycle.sh
+++ cycle.sh
@@ -1,6 +1,8 @@
-for i in ~/datasrc/wallpaper-art/original/*png; do
+#for i in ~/datasrc/wallpaper-art/original/*{png,jpg}; do
+for i in ~/datasrc/wallpaper-art/original/*jpg; do
 	echo $i
-	swaymsg output "eDP-1" bg ${i} fill
+	#swaymsg output "eDP-1" bg ${i} fill
+	swaymsg output "DP-1" bg ${i} fill
 	read key <&1
 	if [ "$key" = q ] ; then
 		break
